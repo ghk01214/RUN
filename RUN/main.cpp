d9ef9ed1ae16155d42eb4c0c2ed185fcd8698c3f
Yun-Joo-Seong <112536317+Yun-Joo-Seong@users.noreply.github.com>
2022-12-01 18:42:22 +0900

    요약 : 커밋 메세지 작성 연습
    사유 : 테스트중

--- RUN/main.cpp
+++ RUN/main.cpp
@@ -9,10 +9,10 @@ int32_t main(int32_t argc, char** argv)
 	// glut 초기화
 	glutInit(&argc, argv);
 
-	// test 용
-
 	// 게임을 표시할 윈도우 크기 결정
 	const Window window{ 800, 800, true };
+
+	// 아아 테스트
 
 	// 게임 엔진 초기화
 	engine.Init(&window);
@@ -24,3 +24,5 @@ int32_t main(int32_t argc, char** argv)
 
 	engine.Update();
 }
+
+// 풀은 어떻게 하는걸까
